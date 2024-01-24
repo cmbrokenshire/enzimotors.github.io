699b7f5eebddaaf74b0f431464160a9da3445bc1
Christopher Brokenshire <cmbrokenshire@wm.edu>
2024-01-24 14:42:10 -0500

    initial app to deploy

---
+++ server.js
@@ -0,0 +1,33 @@
+// Using ES6 import syntax
+import express from 'express';
+import fetch from 'node-fetch';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static('public')); // Serve static files from 'public' directory
+
+app.post('/getBatteryVoltage', async (req, res) => {
+    const serialNumber = req.body.serialNumber;
+    try {
+        const response = await fetch(`http://164.92.185.10/api/battery_voltage/${serialNumber}/`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Token 8e3120cd6f63acb25abef3a4ab421626adbd95e6'
+            }
+        });
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        res.send(data);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Error fetching data');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
